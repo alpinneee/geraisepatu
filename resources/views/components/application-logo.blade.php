bb07257eb04f169bcdba561612a5fd407c983714
alpineee <m.alfin.z117@gmail.com>
2025-09-25 11:20:40 +0700

    Fix logo path with direct URL and htaccess rule

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,2 +1,1 @@
-<img src="{{ asset('logo.png') }}" alt="Logo" {{ $attributes }} onerror="console.log('Logo failed to load:', this.src); this.style.display='none'; this.nextElementSibling.style.display='inline-block';">
-<span style="display:none; font-weight:bold; color:#1f2937; font-size:1.2em;">TOKO SEPATU</span>
+<img src="{{ url('logo.png') }}" alt="Logo" {{ $attributes }}>
