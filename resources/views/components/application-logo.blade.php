fa03ec1e8b63efb2ae1775dda825c243a0371724
alpineee <m.alfin.z117@gmail.com>
2025-09-25 11:17:16 +0700

    Fix logo display with fallback and static file access

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,1 +1,2 @@
-<img src="{{ asset('logo.png') }}" alt="Logo" {{ $attributes }}>
+<img src="{{ asset('logo.png') }}" alt="Logo" {{ $attributes }} onerror="console.log('Logo failed to load:', this.src); this.style.display='none'; this.nextElementSibling.style.display='inline-block';">
+<span style="display:none; font-weight:bold; color:#1f2937; font-size:1.2em;">TOKO SEPATU</span>
