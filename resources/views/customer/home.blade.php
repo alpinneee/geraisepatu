8f6bcde23d6deb3e1fd2fbc349f2474aedfbe34d
alpineee <m.alfin.z117@gmail.com>
2025-10-06 16:06:52 +0700

    Reduce banner height to smaller size

--- resources/views/customer/home.blade.php
+++ resources/views/customer/home.blade.php
@@ -11,7 +11,7 @@
     <div class="relative overflow-hidden pt-10">
         <div id="carousel" class="flex transition-transform duration-500 ease-in-out">
             @foreach($banners as $banner)
-            <div class="w-full flex-shrink-0 relative h-80 sm:h-96 md:h-[500px] lg:h-[600px]">
+            <div class="w-full flex-shrink-0 relative h-64 sm:h-72 md:h-80 lg:h-96">
                 <img class="w-full h-full object-cover" 
                      src="{{ asset('storage/' . $banner->image) }}" 
                      alt="{{ $banner->title }}">
