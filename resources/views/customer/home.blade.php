6d4764005a233b30d2f3525b5acd93b48913af59
alpineee <m.alfin.z117@gmail.com>
2025-10-06 16:13:23 +0700

    Fix

--- resources/views/customer/home.blade.php
+++ resources/views/customer/home.blade.php
@@ -11,7 +11,7 @@
     <div class="relative overflow-hidden pt-10">
         <div id="carousel" class="flex transition-transform duration-500 ease-in-out">
             @foreach($banners as $banner)
-            <div class="w-full flex-shrink-0 relative h-74 sm:h-82 md:h-90 lg:h-106">
+            <div class="w-full flex-shrink-0 relative h-64 sm:h-72 md:h-80 lg:h-96">
                 <img class="w-full h-full object-cover" 
                      src="{{ asset('storage/' . $banner->image) }}" 
                      alt="{{ $banner->title }}">
